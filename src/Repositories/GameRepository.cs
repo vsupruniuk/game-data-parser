1fc6784049c7ce5476c57d34528933258522d025
Vlad Supruniuk <vladsupruniuk@gmail.com>
2023-07-10 15:56:08 +0300

    Small improvements

--- src/Repositories/GameRepository.cs
+++ src/Repositories/GameRepository.cs
@@ -6,7 +6,6 @@ namespace game_data_parser.Repositories;
 
 public class GameRepository : IGameRepository
 {
-
 	public bool IsFileExist(string filePath)
 	{
 		bool isExist = File.Exists(filePath);
@@ -30,10 +29,11 @@ public class GameRepository : IGameRepository
 		}
 		catch (JsonException ex)
 		{
+			ConsoleColor originalColor = Console.ForegroundColor;
 			Console.WriteLine($"JSON in the {filePath} was not in a valid format. JSON body:");
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine(fileData);
-			Console.ForegroundColor = ConsoleColor.White;
+			Console.ForegroundColor = originalColor;
 			Console.WriteLine("Sorry! The application has experienced an unexpected error and will have to be closed.");
 
 			throw;
@@ -49,9 +49,9 @@ public class GameRepository : IGameRepository
 			Console.WriteLine("No games are present in the input file.");
 			return;
 		}
-		
+
 		Console.WriteLine("Loaded games are:");
-		
+
 		foreach (Game game in games)
 		{
 			Console.WriteLine($"{game.Title}, released in {game.ReleaseYear}, rating: {game.Rating}");
--- src/Types/Game.cs
+++ src/Types/Game.cs
@@ -2,7 +2,7 @@ namespace game_data_parser.Types;
 
 public class Game
 {
-	public string Title { get; set; }
-	public int ReleaseYear { get; set; }
-	public float Rating { get; set; }
+	public string Title { get; init; }
+	public int ReleaseYear { get; init; }
+	public decimal Rating { get; init; }
 }
